5b3d829e1244f6e6a1b596ba99c0dd7aa4760fcf
NissimBet <nissimbet123@outlook.com>
2020-02-05 08:02:32 -0600

    parte 1 / 2 finish

--- moviedex-server/index.js
+++ moviedex-server/index.js
@@ -1,65 +1,103 @@
-let express = require( 'express' );
-let bodyParser = require( 'body-parser' );
-let mongoose = require( 'mongoose' );
+let express = require("express");
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
 let jsonParser = bodyParser.json();
-let { DATABASE_URL, PORT } = require( './config' );
+
+const { getMovies, createMovie } = require("./model");
+
+let { DATABASE_URL, PORT } = require("./config");
 
 let app = express();
+app.use(jsonParser);
 
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-	res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-	if (req.method === "OPTIONS") {
-		return res.send(204);
-	}
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  if (req.method === "OPTIONS") {
+    return res.send(204);
+  }
+  next();
 });
 
-/* Tu código va aquí */
+app.get("/api/moviedex", async (_, res) => {
+  try {
+    const allMovies = await getMovies();
+
+    return res.status(200).json(allMovies);
+  } catch (error) {
+    console.log(error);
+    res.statusMessage = "Server error";
+    return res.sendStatus(500);
+  }
+});
+
+app.post("/api/moviedex", async (req, res) => {
+  try {
+    const { title, year, rating } = req.body;
+    if (title && year && rating) {
+      const movie = await createMovie({
+        film_title: title,
+        year: year,
+        rating: rating
+      });
+
+      res.statusMessage = "Movie entry created successfuly";
+      return res.status(201).json(movie);
+    }
+    res.statusMessage = "Missing Parameters";
+    return res.sendStatus(406);
+  } catch (error) {
+    console.log(error);
+    res.statusMessage = "Server error";
+    return res.status(500);
+  }
+});
 
 let server;
 
-function runServer( port, databaseUrl ){
-	return new Promise( (resolve, reject ) => {
-		mongoose.connect( databaseUrl,  { useNewUrlParser: true, useUnifiedTopology: true  }, response => {
-			if ( response ){
-				return reject( response );
-			}
-			else{
-				server = app.listen(port, () => {
-					console.log( "App is running on port " + port );
-					resolve();
-				})
-				.on( 'error', err => {
-					mongoose.disconnect();
-					return reject(err);
-				})
-			}
-		});
-	});
+function runServer(port, databaseUrl) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(
+      databaseUrl,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      response => {
+        if (response) {
+          return reject(response);
+        } else {
+          server = app
+            .listen(port, () => {
+              console.log("App is running on port " + port);
+              resolve();
+            })
+            .on("error", err => {
+              mongoose.disconnect();
+              return reject(err);
+            });
+        }
+      }
+    );
+  });
 }
 
-function closeServer(){
-	return mongoose.disconnect()
-		.then( () => {
-			return new Promise( (resolve, reject) => {
-				console.log( 'Closing the server' );
-				server.close( err => {
-					if ( err ){
-						return reject( err );
-					}
-					else{
-						resolve();
-					}
-				});
-			});
-		});
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing the server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  });
 }
-runServer( PORT, DATABASE_URL );
+runServer(PORT, DATABASE_URL);
 
-module.exports = { 
-    app, 
-    runServer, 
-    closeServer 
-}
+module.exports = {
+  app,
+  runServer,
+  closeServer
+};
--- moviedex-server/model.js
+++ moviedex-server/model.js
@@ -1,10 +1,32 @@
-let mongoose = require( 'mongoose' );
-let uuid = require( 'uuid' );
+let mongoose = require("mongoose");
+let uuid = require("uuid");
 
 mongoose.Promise = global.Promise;
 
-/* Tu código va aquí */
+const MovieModelSchema = new mongoose.Schema({
+  film_id: { type: String, default: uuid.v4() },
+  film_title: String,
+  year: Number,
+  rating: Number
+});
+
+const Movie = mongoose.model("movies", MovieModelSchema);
 
 module.exports = {
-    
+  createMovie: async ({ film_title, year, rating }) => {
+    try {
+      const createdMovie = await Movie.create({ film_title, year, rating });
+      return createdMovie;
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+  getMovies: async () => {
+    try {
+      const allMovies = await Movie.find();
+      return allMovies;
+    } catch (error) {
+      throw Error(error);
+    }
+  }
 };
